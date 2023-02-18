4b8feaad7996b90f5d5b9e7f9b9986f106ff6eb6
JagDisH2018 <jagdishsurin6266@gmail.com>
2023-02-18 21:48:57 +0530

    first commit

---
+++ src/main/java/firstGithub/demo_firstGithub/App.java
@@ -0,0 +1,13 @@
+package firstGithub.demo_firstGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/firstGithub/demo_firstGithub/AppTest.java
@@ -0,0 +1,20 @@
+package firstGithub.demo_firstGithub;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
